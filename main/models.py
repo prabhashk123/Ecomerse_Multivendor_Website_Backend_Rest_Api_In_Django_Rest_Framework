88ec269c36fa8c0314ecf51a175d298ca5119013
prabhashk123 <sunriseprabhash.bh@gmail.com>
2024-01-30 01:29:35 +0530

    fixed contactus model

--- main/models.py
+++ main/models.py
@@ -206,14 +206,14 @@ class Notification(models.Model):
 class Contact(models.Model):
     name=models.CharField(max_length=100,null=True)
     email=models.EmailField(null=True)
-    phone=models.IntegerField(max_length=10,null=True)
+    phone=models.IntegerField(max_length=50,null=True)
     address=models.TextField(max_length=200,null=True)
     query=models.CharField(max_length=200,null=True)
     countary=models.CharField(max_length=100,null=True)
     code=models.CharField(max_length=10,null=True)
     state=models.CharField(max_length=100,null=True)
     city=models.CharField(max_length=100,null=True)
-    pincode=models.IntegerField(max_length=6)
+    pincode=models.IntegerField(null=True)
     querydetail=models.TextField(max_length=200,null=True)
 
     def __str__(self):
