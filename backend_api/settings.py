c661a4c5bd7d260daf0e7c09e8b7efb138d83f2e
Prabhash kumar <82327219+prabhashk123@users.noreply.github.com>
2024-02-19 16:33:27 +0530

    Update settings.py
    add DISABLE_COLLECTSTATIC=1

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -173,6 +173,7 @@ EMAIL_PORT=587
 EMAIL_HOST_USER='prabhashbhagat1999@gmail.com'
 EMAIL_HOST_PASSWORD='lwzv zqdz uczk apwb'
 # for heroque
+DISABLE_COLLECTSTATIC=1
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
